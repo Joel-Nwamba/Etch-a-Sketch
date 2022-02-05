65f45a1dcf37a8f1368cbc703fd0630e4844f7e3
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-02-04 19:58:12 -0500

    class not modifying

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 const container = document.getElementById("container");
+const button = document.querySelector(".btn-grid");
+let row = 16;
+let col = 16
 
 
 // create a grid 
-function createGrid(rows, cols) {
+function createGrid() {
     container.style.setProperty("color", "blue");
-    for(let i = 0; i < (rows * cols); i++) {
+    for(let i = 0; i < (row * col); i++) {
         const grid = document.createElement("div");
+        grid.style.backgroundColor = "#972900";
+        grid.style.width = "30px";
+        grid.style.height = "28px";
+        grid.style.border = "5px solid blue";
         grid.classList.add("square")
         container.appendChild(grid);
     }
 }
 
-createGrid(16, 16);
+createGrid();
