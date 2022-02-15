57cd0c0b53864e1a302a726715be51646278239f
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-02-14 20:40:45 -0500

    Need to update prompt

--- js/index.js
+++ js/index.js
@@ -1,30 +1,33 @@
 const container = document.getElementById("container");
 const clearButton = document.getElementById("clear-grid");
+const size = document.getElementById("size");
 let row = 16;
 let col = 16;
-let squares = [];
+
 
 
 // create a grid 
 function createGrid() {
-    container.style.setProperty("color", "blue");
-    for(let i = 0; i < (row * col); i++) {
+    for(let i=0; i < (row * col); i++) {
         const grid = document.createElement("div");
         grid.addEventListener("mouseover", function(e) {
             e.target.classList.add("square");
         })
-        grid.textContent = "X";
         grid.style.width = "30px";
-        grid.style.height = "25px";
-        grid.style.border = "5px solid blue";
+        grid.style.height="25px";
         container.appendChild(grid);
-        squares.push(grid);
         clearButton.addEventListener("click", function() {
-            grid.style.backgroundColor = "none";
-            grid.classList.remove("square");
-        })
+            grid.classList.remove("square")
+        });
     }
-
 }
 
 createGrid();
+let prompUser=
+size.addEventListener("click", function() {
+   prompUser=prompt("Choose between 1-100");
+   
+
+
+})
+
