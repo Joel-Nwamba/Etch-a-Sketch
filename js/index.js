36023cb256bddfb0d1422a7c53cb01fbc6d150e9
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-03-01 17:35:42 -0500

    Add option to choose grid size

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 const container = document.getElementById("container");
 const clearButton = document.getElementById("clear-grid");
 const changeSize = document.getElementById("size");
-let sizeLength = 16;
+let sizeGrid = 16;
 // let row = 16;
 // let col = 16;
 container.style.gridTemplateColumns = (`repeat( ${sizeLength}, 1fr)`)
 // create a grid 
 function generateGrid(size) {
-    getNewSize() 
-//    newFunction();
+    if (size > 100) {
+        window.alert("100 is the max height");
+         sizeGrid = 100;
+    }
     for(let i=0; i < size; i++) {
         const rowcol = document.createElement("div");
         rowcol.classList.add("row")
@@ -28,33 +30,10 @@ function generateGrid(size) {
             container.appendChild(rowcol)
     }
 
-    function getNewSize() {
-        let sizeLength;
-        if (size > 100) {
-            alert("Maximum is the number 100");
-            sizeLength = 100;
-        } else {
-            sizeLength = size;
-        }
-    }
 }
 
-// function getNewSize() {
-//     changeSize.addEventListener("click", function() {
-//         let usersChoice = prompt("What size would you like to go with");
-//         if(parseInt(usersChoice) > 100) {
-//             return 100;
-//         } else {
-//             return parseInt(usersChoice);
-//         }
-//     })
-// }
+function chooseGridSize() {
+    
+}
 
-// let size = getNewSize()
-
-
-
-generateGrid(16)
-
-
-
+generateGrid(16);
