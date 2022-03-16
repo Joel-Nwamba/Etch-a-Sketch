a1b314a8782ffb9e949329ea40680ab8dc63ba10
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-03-15 20:50:18 -0400

    Update random numbers appear on squares

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const container = document.getElementById("container");
 const clearButton = document.getElementById("clear-grid");
 const changeSize = document.getElementById("size");
 const square = document.createElement("div");
+const colorChange = document.getElementById("change-color");
 // create a grid 
 function generateGrid(size) {
     let containerSize = Number(960);
@@ -17,6 +18,10 @@ function generateGrid(size) {
                 square.addEventListener("mouseover", () => {
                         square.style.backgroundColor = "black";
                     })
+                    colorChange.addEventListener("click", function() {
+                        square.addEventListener("mouseover", changeGridColor)
+                    })
+                    // square.addEventListener("mouseover", changeGridColor)
                 clearButton.addEventListener("click", function() {
                     square.style.backgroundColor = "white";
                 })
@@ -29,6 +34,16 @@ function generateGrid(size) {
     }
 
 }
+
+function changeGridColor(event) {
+    let a = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let c = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+    event.target.style.opacity = 0.1;
+}
+
+
 
 changeSize.addEventListener("click", function() {
     let usersChoice = prompt(`"How man squares per side would you like? (maximum length is 100)`);
